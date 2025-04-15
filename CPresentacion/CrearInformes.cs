84a451f95cb29ed668227045be3d585722a4e6b3
celeste-q15 <150865742+celeste-q15@users.noreply.github.com>
2025-04-14 22:41:53 -0300

    Informepresentacion

---
+++ CPresentacion/CrearInformes.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultorioPsicopedagogico.CPresentacion
+{
+    public partial class CrearInformes : Form
+    {
+        public CrearInformes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardarPDF_Click_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+           
+        
+
+        private void volver_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }           
+}
+      
+      
+
+
